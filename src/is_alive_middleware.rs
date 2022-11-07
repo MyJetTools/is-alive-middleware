f06ba8e50e73e182d5d525851ecdcfac48f48cd1
amigin <amigin@gmail.com>
2022-11-07 23:08:21 +0200

    Updating versions

--- src/is_alive_middleware.rs
+++ src/is_alive_middleware.rs
@@ -1,18 +1,15 @@
 use my_http_server::{
     HttpContext, HttpFailResult, HttpOkResult, HttpOutput, HttpServerMiddleware,
-    HttpServerRequestFlow, RequestCredentials,
+    HttpServerRequestFlow,
 };
 use rust_extensions::date_time::DateTimeAsMicroseconds;
 use serde::{Deserialize, Serialize};
 
-pub struct IsAliveMiddleware<TRequestCredentials: RequestCredentials + Send + Sync + 'static> {
+pub struct IsAliveMiddleware {
     is_alive: IsAliveContract,
-    _a: Option<TRequestCredentials>,
 }
 
-impl<TRequestCredentials: RequestCredentials + Send + Sync + 'static>
-    IsAliveMiddleware<TRequestCredentials>
-{
+impl IsAliveMiddleware {
     pub fn new(app_name: String, app_version: String) -> Self {
         let env_info = if let Ok(env_info) = std::env::var("ENV_INFO") {
             Some(env_info)
@@ -27,20 +24,16 @@ impl<TRequestCredentials: RequestCredentials + Send + Sync + 'static>
                 env_info,
                 started: DateTimeAsMicroseconds::now().unix_microseconds,
             },
-            _a: None,
         }
     }
 }
 
 #[async_trait::async_trait]
-impl<TRequestCredentials: RequestCredentials + Send + Sync + 'static> HttpServerMiddleware
-    for IsAliveMiddleware<TRequestCredentials>
-{
-    type TRequestCredentials = TRequestCredentials;
+impl HttpServerMiddleware for IsAliveMiddleware {
     async fn handle_request(
         &self,
-        ctx: &mut HttpContext<Self::TRequestCredentials>,
-        get_next: &mut HttpServerRequestFlow<TRequestCredentials>,
+        ctx: &mut HttpContext,
+        get_next: &mut HttpServerRequestFlow,
     ) -> Result<HttpOkResult, HttpFailResult> {
         if ctx
             .request
